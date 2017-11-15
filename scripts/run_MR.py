42d70f04c3352d9891af62f9604acde799d3e001
jpdeleon <jpdeleon.bsap@gmail.com>
2017-11-15 16:48:38 +0900

    fixed RV plot labels

--- scripts/run_MR.py
+++ scripts/run_MR.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python
 '''
 Created: 11/09
-Input: c10_per_rad_stm.csv; run_MR.R
+Updated: 11/14
+
+Input: c10_per_rad_stm_kp.csv; run_MR.R
 run_MR.R requires:
 posterior_samples.savr; calc_mass_postpred.R
 
-Output: KESPRINT-C10/*.ps & .txt
+Output: KESPRINT-C10/*.png & .txt
 
-Purpose: estimtae mass of KESPRINT validated campaign 10
-systems using MRrelation (Wolfgang 2017)
+Purpose: estimate mass of KESPRINT validated campaign 10
+systems using MRrelation (Wolfgang 2015)
 
 see also mass estimation of KESPRINT targets using M-R relation.ipynb
 
 This script reads the list of validated planets (epic)
 with their radius derived from transit observation.
 
-The epics number, radius, and radius error is fed
-as argument to the run_MR.R script which produces 
-the .ps plot and posterior 
+The epic number, planet letter, radius, and radius error 
+are fed as arguments to the run_MR.R script which produces 
+the plots (.png) and posterior samples of mass prediction (.txt) 
 The R script follows the example in github.
 '''
 
@@ -34,4 +36,7 @@ radii_errs = df['r_err']
 n=-1 #run all
 for epic,let,r,r_err in zip(epics[:n],letter[:n],radii[:n],radii_errs[:n]):
     #print(epic)
+    ###verbose
     !Rscript run_MR.R $epic $let $r $r_err
+    ###use below to hide output in terminal
+    !Rscript run_MR.R $epic $let $r $r_err 2&>1 >/dev/null
